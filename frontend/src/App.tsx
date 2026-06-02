0561f29e6434e7ad1b2145ae432b0e77873f3f9f
Dhruvin-cmt <dhruvin@voicenova.ai>
2026-06-02 12:53:18 +0530

    again fix naming bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ import { Role } from "./types/authtype";
 import { useEffect } from "react";
 import { socket } from "./socket/socket";
 import BloglistPage from "./pages/Blog/BloglistPage";
-import SingleBlogPage from "./pages/Blog/BlogPage";
+import BlogPage from "./pages/Blog/BlogPage";
 
 export default function App() {
 
@@ -39,7 +39,7 @@ export default function App() {
             }
           />
           <Route path="/blogs" element={<BloglistPage />} />
-          <Route path="/blog/:id" element={<SingleBlogPage />} />
+          <Route path="/blog/:id" element={<BlogPage />} />
           <Route
             path="/blog/publish"
             element={
